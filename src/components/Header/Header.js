79d3f26aa6bd6a10b15613654bf8cfbbcad63368
Alexandre Leite <alexandre@bluebi.com.br>
2025-01-13 11:51:34 -0300

    alterações no navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,20 +14,27 @@ function Header() {
         <button className="menu-button" onClick={toggleMenu}>
           <i className="fas fa-bars"></i>
         </button>
-        <div className="brand">
+        <div className="brand-titule">
           <h1>MaeLu Bordados</h1>
+          <div className="brand">
           <h1>Bordados personalizados e feitos com muito amor!</h1>
+        </div>
         </div>
         <div className="cart">
           <i className="fas fa-shopping-cart"></i>
         </div>
       </div>
-      <nav className={`sidebar ${isMenuOpen ? 'open' : ''}`}>
+      <nav className={`sidebar  ${isMenuOpen ? 'open' : ''}` }>
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Produtos</a></li>
-          <li><a href="#">Sobre Nós</a></li>
-          <li><a href="#">Contato</a></li>
+          <li><a href="#">Bordados</a></li>
+          <li><a href="#">Bory bebê</a></li>
+          <li><a href="#">Fraldas</a></li>
+          <li><a href="#">Marca página</a></li>
+          <li><a href="#">Toalhas de bebê</a></li>
+          <li><a href="#">Toalhas de escolinha</a></li>
+          <li><a href="#">Talha de lavado</a></li>
+          <li><a href="#">Toalha de rosto</a></li>
+          <li><a href="#">Últimos lançamentoscls</a></li>
         </ul>
       </nav>
     </header>
